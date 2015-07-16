a481c7304bfb192d0ac1875da592ab141ddf8495
Daniele Megna <megna.dany@gmail.com>
2015-07-16 08:06:49 +0200

    Replaced tab with spaces uniforming indentation

--- TennisGame1.php
+++ TennisGame1.php
@@ -4,40 +4,40 @@ require_once "TennisGame.php";
 
 class TennisGame1 implements TennisGame
 {
-	private $player1Score = 0;
-	private $player2Score = 0;
-	private $player1Name = '';
-	private $player2Name = '';
+  private $player1Score = 0;
+  private $player2Score = 0;
+  private $player1Name = '';
+  private $player2Name = '';
 
-	public function __construct($player1Name, $player2Name)
-	{
-		$this->player1Name = $player1Name;
-		$this->player2Name = $player2Name;
-	}
+  public function __construct($player1Name, $player2Name)
+  {
+    $this->player1Name = $player1Name;
+    $this->player2Name = $player2Name;
+  }
 
-	public function wonPoint($playerName)
-	{
-		if($playerName == $this->player1Name) {
-			$this->player1Score++;
+  public function wonPoint($playerName)
+  {
+    if($playerName == $this->player1Name) {
+      $this->player1Score++;
       return;
     }
     
     $this->player2Score++;
-	}
+  }
 
-	public function getScore()
-	{
-		if ($this->player1Score == $this->player2Score) {
+  public function getScore()
+  {
+    if ($this->player1Score == $this->player2Score) {
       $tieScore = $this->player1Score;
 
       if($tieScore > 2)
         return "Deuce";
       
       return $this->scoreToString($tieScore)."-All";
-		}
+    }
     
     if ($this->player1Score > 3 || $this->player2Score > 3) {
-			$scoreDiff = $this->player1Score - $this->player2Score;
+      $scoreDiff = $this->player1Score - $this->player2Score;
       if ($scoreDiff == 1)
         return "Advantage " . $this->player1Name;
       if ($scoreDiff == -1)
@@ -46,11 +46,11 @@ class TennisGame1 implements TennisGame
         return "Win for " . $this->player1Name;
 
       return "Win for " . $this->player2Name;
-		}
+    }
 
     return $this->scoreToString($this->player1Score)
       . "-" . $this->scoreToString($this->player2Score);
-	}
+  }
 
   private function scoreToString($score) {
     switch ($score) {
