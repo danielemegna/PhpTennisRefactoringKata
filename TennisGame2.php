b8d023db4edd62264423a0e1147dc848bce4a9d9
Daniele Megna <megna.dany@gmail.com>
2015-07-18 16:54:03 +0200

    Introduced Player class separating responsabilites

--- TennisGame2.php
+++ TennisGame2.php
@@ -4,22 +4,18 @@ require_once "TennisGame.php";
 
 class TennisGame2 implements TennisGame
 {
-  private $P1name;
-  private $P2name;
-  private $P1point;
-  private $P2point;
+  private $player1;
+  private $player2;
 
   public function __construct($P1name, $P2name)
   {
-    $this->P1name = $P1name;
-    $this->P2name = $P2name;
-    $this->P1point = 0;
-    $this->P2point = 0;
+    $this->player1 = new Player($P1name);
+    $this->player2 = new Player($P2name);
   }
 
   public function wonPoint($player)
   {
-    if ($player == $this->P1name)
+    if ($player == $this->player1->getName())
       $this->P1Score();
     else
       $this->P2Score();
@@ -27,29 +23,29 @@ class TennisGame2 implements TennisGame
 
   public function getScore()
   {
-    $pointDiff = ($this->P1point - $this->P2point);
-    if ($this->P1point > 3 && $pointDiff > 1)
-      return "Win for " . $this->P1name;
+    $pointDiff = ($this->player1->getScore() - $this->player2->getScore());
+    if ($this->player1->getScore() > 3 && $pointDiff > 1)
+      return "Win for " . $this->player1->getName();
 
-    if ($this->P2point > 3 && $pointDiff < -1)
-      return "Win for " . $this->P2name;
+    if ($this->player2->getScore() > 3 && $pointDiff < -1)
+      return "Win for " . $this->player2->getName();
 
-    if ($this->P1point > 2 && $pointDiff < 0)
-      return "Advantage " . $this->P2name;
+    if ($this->player1->getScore() > 2 && $pointDiff < 0)
+      return "Advantage " . $this->player2->getName();
 
-    if ($this->P2point > 2 && $pointDiff > 0)
-      return "Advantage " . $this->P1name;
+    if ($this->player2->getScore() > 2 && $pointDiff > 0)
+      return "Advantage " . $this->player1->getName();
 
 
     if ($pointDiff == 0) {
-      if($this->P1point >= 3)
+      if($this->player1->getScore() >= 3)
         return "Deuce";
 
-      $p1ScoreToString = $this->scoreToString($this->P1point);
+      $p1ScoreToString = $this->scoreToString($this->player1->getScore());
       $p2ScoreToString = "All";
     } else {
-      $p1ScoreToString = $this->scoreToString($this->P1point);
-      $p2ScoreToString = $this->scoreToString($this->P2point);
+      $p1ScoreToString = $this->scoreToString($this->player1->getScore());
+      $p2ScoreToString = $this->scoreToString($this->player2->getScore());
     }
 
     return "{$p1ScoreToString}-{$p2ScoreToString}";
@@ -64,15 +60,39 @@ class TennisGame2 implements TennisGame
       return "Thirty";
     if ($score == 3)
       return "Forty";
+
+    throw new Exception("Cannot get string for score '$score'");
   }
 
   private function P1Score()
   {
-    $this->P1point++;
+    $this->player1->increaseScore();
   }
 
   private function P2Score()
   {
-    $this->P2point++;
+    $this->player2->increaseScore();
+  }
+}
+
+class Player {
+  private $name;
+  private $score;
+
+  public function __construct($name) {
+    $this->name = $name;
+    $this->score = 0;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getScore() {
+    return $this->score;
+  }
+
+  public function increaseScore() {
+    $this->score++;
   }
 }
