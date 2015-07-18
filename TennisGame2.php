55f3ddc0aa9f008e99787f26be6c710834dfc4bc
Daniele Megna <megna.dany@gmail.com>
2015-07-18 17:00:21 +0200

    Removed duplication in getScore method

--- TennisGame2.php
+++ TennisGame2.php
@@ -23,29 +23,32 @@ class TennisGame2 implements TennisGame
 
   public function getScore()
   {
-    $pointDiff = ($this->player1->getScore() - $this->player2->getScore());
-    if ($this->player1->getScore() > 3 && $pointDiff > 1)
+    $player1Score = $this->player1->getScore();
+    $player2Score = $this->player2->getScore();
+
+    $pointDiff = ($player1Score - $player2Score);
+    if ($player1Score > 3 && $pointDiff > 1)
       return "Win for " . $this->player1->getName();
 
-    if ($this->player2->getScore() > 3 && $pointDiff < -1)
+    if ($player2Score > 3 && $pointDiff < -1)
       return "Win for " . $this->player2->getName();
 
-    if ($this->player1->getScore() > 2 && $pointDiff < 0)
+    if ($player1Score > 2 && $pointDiff < 0)
       return "Advantage " . $this->player2->getName();
 
-    if ($this->player2->getScore() > 2 && $pointDiff > 0)
+    if ($player2Score > 2 && $pointDiff > 0)
       return "Advantage " . $this->player1->getName();
 
 
     if ($pointDiff == 0) {
-      if($this->player1->getScore() >= 3)
+      if($player1Score >= 3)
         return "Deuce";
 
-      $p1ScoreToString = $this->scoreToString($this->player1->getScore());
+      $p1ScoreToString = $this->scoreToString($player1Score);
       $p2ScoreToString = "All";
     } else {
-      $p1ScoreToString = $this->scoreToString($this->player1->getScore());
-      $p2ScoreToString = $this->scoreToString($this->player2->getScore());
+      $p1ScoreToString = $this->scoreToString($player1Score);
+      $p2ScoreToString = $this->scoreToString($player2Score);
     }
 
     return "{$p1ScoreToString}-{$p2ScoreToString}";
