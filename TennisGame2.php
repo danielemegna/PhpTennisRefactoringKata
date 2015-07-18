478a40c386efe6d082290e241b408165a9fc8ca3
Daniele Megna <megna.dany@gmail.com>
2015-07-18 18:31:31 +0200

    Semplified getScore algorhythm

--- TennisGame2.php
+++ TennisGame2.php
@@ -11,10 +11,10 @@ class TennisGame2 implements TennisGame
 
   private $scoreFormatter;
 
-  public function __construct($P1name, $P2name)
+  public function __construct($player1Name, $player2Name)
   {
-    $this->player1 = new Player($P1name);
-    $this->player2 = new Player($P2name);
+    $this->player1 = new Player($player1Name);
+    $this->player2 = new Player($player2Name);
 
     $this->scoreFormatter = new ScoreFormatter();
   }
@@ -33,25 +33,25 @@ class TennisGame2 implements TennisGame
   {
     $player1Score = $this->player1->getScore();
     $player2Score = $this->player2->getScore();
-    $scoreDiff = ($player1Score - $player2Score);
-
-    if ($scoreDiff == 0) {
-      if($player1Score >= 3)
+    
+    if ($player1Score == $player2Score) {
+      if($player1Score > 2)
         return $this->scoreFormatter->deuceString();
 
       return $this->scoreFormatter->tieString($player1Score);
     }
-
+    
+    $scoreDiff = ($player1Score - $player2Score);
     if ($player1Score > 3 && $scoreDiff > 1)
       return $this->scoreFormatter->winString($this->player1->getName());
 
     if ($player2Score > 3 && $scoreDiff < -1)
       return $this->scoreFormatter->winString($this->player2->getName());
 
-    if ($player1Score > 2 && $scoreDiff < 0)
+    if ($player1Score > 2 && $scoreDiff == -1)
       return $this->scoreFormatter->advantageString($this->player2->getName());
 
-    if ($player2Score > 2 && $scoreDiff > 0)
+    if ($player2Score > 2 && $scoreDiff == 1)
       return $this->scoreFormatter->advantageString($this->player1->getName());
 
     $p1ScoreToString = $this->scoreFormatter->scoreToString($player1Score);
