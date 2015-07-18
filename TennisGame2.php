ff9f1889888d3746ea6b589fac920bc150505496
Daniele Megna <megna.dany@gmail.com>
2015-07-18 12:22:51 +0200

    Uniformed similar if cases to the same code

--- TennisGame2.php
+++ TennisGame2.php
@@ -36,6 +36,8 @@ class TennisGame2 implements TennisGame
     }
 
     if ($this->P1point > 0 && $this->P2point == 0) {
+      if ($this->P1point == 0)
+        $this->P1res = "Love";
       if ($this->P1point == 1)
         $this->P1res = "Fifteen";
       if ($this->P1point == 2)
@@ -43,42 +45,81 @@ class TennisGame2 implements TennisGame
       if ($this->P1point == 3)
         $this->P1res = "Forty";
 
-      $this->P2res = "Love";
+      if ($this->P2point == 0)
+        $this->P2res = "Love";
+      if ($this->P2point == 1)
+        $this->P2res = "Fifteen";
+      if ($this->P2point == 2)
+        $this->P2res = "Thirty";
+      if ($this->P2point == 3)
+        $this->P2res = "Forty";
+
       $score = "{$this->P1res}-{$this->P2res}";
     }
 
     if ($this->P2point > 0 && $this->P1point == 0) {
-      if ($this->P2point == 1)
-        $this->P2res = "Fifteen";
-      if ($this->P2point == 2)
-        $this->P2res = "Thirty";
-      if ($this->P2point == 3)
-        $this->P2res = "Forty";
-      $this->P1res = "Love";
-      $score = "{$this->P1res}-{$this->P2res}";
-    }
-
-    if ($this->P1point > $this->P2point && $this->P1point < 4) {
-      if ($this->P1point == 2)
-        $this->P1res = "Thirty";
-      if ($this->P1point == 3)
-        $this->P1res = "Forty";
-      if ($this->P2point == 1)
-        $this->P2res = "Fifteen";
-      if ($this->P2point == 2)
-        $this->P2res = "Thirty";
-      $score = "{$this->P1res}-{$this->P2res}";
-    }
-
-    if ($this->P2point > $this->P1point && $this->P2point < 4) {
-      if ($this->P2point == 2)
-        $this->P2res = "Thirty";
-      if ($this->P2point == 3)
-        $this->P2res = "Forty";
+      if ($this->P1point == 0)
+        $this->P1res = "Love";
       if ($this->P1point == 1)
         $this->P1res = "Fifteen";
       if ($this->P1point == 2)
         $this->P1res = "Thirty";
+      if ($this->P1point == 3)
+        $this->P1res = "Forty";
+
+      if ($this->P2point == 0)
+        $this->P2res = "Love";
+      if ($this->P2point == 1)
+        $this->P2res = "Fifteen";
+      if ($this->P2point == 2)
+        $this->P2res = "Thirty";
+      if ($this->P2point == 3)
+        $this->P2res = "Forty";
+
+      $score = "{$this->P1res}-{$this->P2res}";
+    }
+
+    if ($this->P1point > $this->P2point && $this->P1point < 4) {
+      if ($this->P1point == 0)
+        $this->P1res = "Love";
+      if ($this->P1point == 1)
+        $this->P1res = "Fifteen";
+      if ($this->P1point == 2)
+        $this->P1res = "Thirty";
+      if ($this->P1point == 3)
+        $this->P1res = "Forty";
+
+      if ($this->P2point == 0)
+        $this->P2res = "Love";
+      if ($this->P2point == 1)
+        $this->P2res = "Fifteen";
+      if ($this->P2point == 2)
+        $this->P2res = "Thirty";
+      if ($this->P2point == 3)
+        $this->P2res = "Forty";
+
+      $score = "{$this->P1res}-{$this->P2res}";
+    }
+
+    if ($this->P2point > $this->P1point && $this->P2point < 4) {
+      if ($this->P1point == 0)
+        $this->P1res = "Love";
+      if ($this->P1point == 1)
+        $this->P1res = "Fifteen";
+      if ($this->P1point == 2)
+        $this->P1res = "Thirty";
+      if ($this->P1point == 3)
+        $this->P1res = "Forty";
+
+      if ($this->P2point == 0)
+        $this->P2res = "Love";
+      if ($this->P2point == 1)
+        $this->P2res = "Fifteen";
+      if ($this->P2point == 2)
+        $this->P2res = "Thirty";
+      if ($this->P2point == 3)
+        $this->P2res = "Forty";
+
       $score = "{$this->P1res}-{$this->P2res}";
     }
 
